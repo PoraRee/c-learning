83d66a55f34a6ce38fba2dc5048f76533c22be8b
PoraRee <reeinter5@gmail.com>
2026-09-11 00:53:40 +0700

    add ch1 contents

---
+++ contents/code/01_hello_world/01_01_hello_world.c
@@ -0,0 +1,7 @@
+#include<stdio.h>               // <- Import used functions (printf: display text to console)
+
+int main() {                    // <- Define main function, code execution starts here
+    printf("Hello, World!\n");  // <- Display "Hello, World!" to the console
+
+    return 0;                   // <- Return 0 to indicate successful execution
+}
---
+++ contents/documents/01_hello_world/01_01_hello_world.md
@@ -0,0 +1,8 @@
+# Hello, World!
+
+**This is the "Hello, World!" example—the starting point for every programming language—which will help you see the syntax of the C language.**
+
+![alt text](../../../assets/01_hello_world/hello_world.png)
+
+you can check example code from [here](../../code/01_hello_world/01_01_hello_world.c)
+
