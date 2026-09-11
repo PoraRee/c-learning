51f0615bf9e3889ec9872c41e2d26d4343015941
PoraRee <reeinter5@gmail.com>
2026-09-12 01:28:24 +0700

    add questions

--- contents/quiz/01_hello_world/problemset.h
+++ contents/quiz/01_hello_world/problemset.h
@@ -10,9 +10,29 @@ Quiz quizzes[] = {
         "Fill in the blank to code can run properly in C:\n\n```main.c\n" COLOR_YELLOW "[#########]" COLOR_RESET "\nint main() {\n    printf(\"Hello, World!\\n\");\n    return 0;\n}\n```\n",
         {"import stdio.h", "from stdio import printf", "void printf(char *str);"},
         "include<stdio.h>"
+    },
+    {
+        "Fill in the blank to code can run properly in C:\n\n```main.c\n#include<stdio.h>\nint main() {\n    " COLOR_YELLOW "[#######]" COLOR_RESET "(\"Hello, World!\\n\");\n    return 0;\n}\n```\n",
+        {"print", "echo", "console.log"},
+        "printf"
+    },
+    {
+        "Fill in the blank to code can run properly in C:\n\n```main.c\n#include<stdio.h>\nint main() {\n    printf(\"Hello, World!\\n\");\n    " COLOR_YELLOW "[#######]" COLOR_RESET "\n}\n```\n",
+        {"exit(0);", "return 1;", "end;"},
+        "return 0;"
+    },
+    {
+        "What printf() function does in C?",
+        {"It reads input from the user.", "It allocates memory for variables.", "It terminates the program."},
+        "It prints the output to the console."
+    },
+    {
+        "Why main function is important in C?",
+        {"It is used to declare variables.", "It is used to include libraries.", "It is used to define data types."},
+        "It is the entry point of the program where execution starts."
     }
 };
 
-size_t quiz_size = sizeof(quizzes) / sizeof(quizzes[0]); // Calculate the number of quizzes
+size_t quiz_size = sizeof(quizzes) / sizeof(quizzes[0]);
 
 #endif
